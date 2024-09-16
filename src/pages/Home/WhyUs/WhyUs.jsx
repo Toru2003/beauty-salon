ce341cabc3605acf6d648cc53a03014eb03dc5f7
Toru2003 <daniilbal2003@gmail.com>
2024-09-16 08:09:17 +0500

    FINAL VERSION 1

--- src/pages/Home/WhyUs/WhyUs.jsx
+++ src/pages/Home/WhyUs/WhyUs.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './WhyUs.module.css';
-import './public/price.svg';
-import './public/discount.svg';
-import './public/like.svg';
-import './public/medal.svg';
-import './public/basket.svg';
-import './public/body.svg';
+import price from  '../../../images/price.svg';
+import discount from  '../../../images/discount.svg';
+import like from  '../../../images/like.svg';
+import medal from  '../../../images/medal.svg';
+import basket from  '../../../images/basket.svg';
+import body from  '../../../images/body.svg';
+
 
 const WhyUs = () => {
   const items = [
-    { id: 'price', title: 'Низкие цены', text: 'У нас самые лучшие и небольшие цены на рынке' },
-    { id: 'discount', title: 'Акции и скидки', text: 'только у нас вы найдете, множество акций и скидок' },
-    { id: 'like', title: 'Отзывы', text: 'Наши клиенты довольны работой наших мастеров' },
-    { id: 'medal', title: 'Качество', text: 'Мы выполняем нашу работу на высоком уровне качества' },
-    { id: 'basket', title: 'Услуги', text: 'У нас есть огромный выбор услуг и предложений для вас' },
-    { id: 'body', title: 'Персонал', text: 'У нас работают самые професиональные сотрудники' },
+    { id: price, title: 'Низкие цены', text: 'У нас самые лучшие и небольшие цены на рынке' },
+    { id: discount, title: 'Акции и скидки', text: 'только у нас вы найдете, множество акций и скидок' },
+    { id: like, title: 'Отзывы', text: 'Наши клиенты довольны работой наших мастеров' },
+    { id: medal, title: 'Качество', text: 'Мы выполняем нашу работу на высоком уровне качества' },
+    { id: basket, title: 'Услуги', text: 'У нас есть огромный выбор услуг и предложений для вас' },
+    { id: body, title: 'Персонал', text: 'У нас работают самые професиональные сотрудники' },
   ];
 
   return (
@@ -24,8 +25,8 @@ const WhyUs = () => {
         {items.map((item) => (
           <div key={item.id} className={styles.card}>
             <img
-              src={`${item.id}.svg`}
-              alt={`${item.id}.svg`}
+              src={`${item.id}`}
+              alt={`${item.id}`}
             />
             <h3>{item.title}</h3>
             <p>{item.text}</p>
